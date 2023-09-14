7555ea0d6eece4dc89781a6afeaf47d3f2847b5d
Nofrialdi <aldi.fasive@gmail.com>
2023-09-14 16:02:18 +0700

    style(style home page): style home page and card
    style home page and card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,16 @@ export default function Home() {
 	console.log(allPokemonData);
 	return (
 		<main>
-			<div>
+			<div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 gap-5">
 				{allPokemonData ? (
 					allPokemonData.map((pokemon: any) => {
 						return (
-							<div key={pokemon.id}>
-								<div>
+							<div className="rounded overflow-hidden shadow-lg" key={pokemon.id}>
+								<div className="w-full">
 									<img src={pokemon.sprites.other.home.front_shiny} alt={pokemon.name} />
 								</div>
 								<div>
-									<h3>{pokemon.name}</h3>
+									<h3 className="font-bold text-xl mb-2">{pokemon.name}</h3>
 									<p>More Detail &nbsp; &rarr;</p>
 								</div>
 							</div>
